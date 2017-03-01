028e4dc3416674602cf6f129b93d57dddaf967d4
BarredGalaxy <BarredGalaxy@users.noreply.github.com>
2017-03-01 11:43:42 -0800

    Update r2d2.js

--- r2d2.js
+++ r2d2.js
@@ -5,30 +5,31 @@
 
 const Discord = require("discord.js"); 
 const client = new Discord.Client();
+const prefix = "$";
 
 //Logging into the bot
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`);
+  console.log("Logged in as ${client.user.username}!");
 });
 
 //Logged in, creating a simple test message and we go against trump.
 
-if (msg.content.includes("trump")) 
+if (msg.content.includes("trump")) {
     msg.react('👎');
     msg.reply('Please dont discuss that horrible man. Ever. Like Ever. At all. Thx ;)')
-
+}
+  
 //Created an IP command. When you do $IP it shows you the IP of the server.
 
 client.on('message', msg => {
-  if (msg.content === '$ip') {
+  if (msg.content.startsWith(prefix + "ip")) {
     msg.reply('The Server IP is **play.nightsofvalor.com');
   }
-});
 
 //Create a simple Rules embed. People need to know the rules.
 
-if (msg.content.startsWith('rules'))
+if (msg.content.startsWith(prefix +'rules'))
     msg.channel.sendMessage(" ", {embed: {
             color: 7013413,
             author: {
@@ -37,5 +38,8 @@ if (msg.content.startsWith('rules'))
             },
             description: "http://nightsofvalor.com/rules",
         }});
+});
+
+//enter token
 
 client.login('token');
