0d1e3b421264f3c693749bd52ccbb514cb6c8a70
BarredGalaxy <BarredGalaxy@users.noreply.github.com>
2017-03-02 08:25:49 -0800

    Update r2d2.js

--- r2d2.js
+++ r2d2.js
@@ -3,23 +3,27 @@
 
 //Taking the discord.js and starting to login. 
 
+//Constants
 const Discord = require("discord.js"); 
 const client = new Discord.Client();
 const prefix = "$";
+//Fun
+const playercards = ["BarredGalaxy [*50%*]", "BarredGalaxy [*50%*]", "BarredGalaxy [*50%*]", "GalaxyIsBroken [*5%*]", "Omanges [*1%*]", "BluePandaDragon [*2%*]", "Neterkun [*10%*]", "Prxncey [*14%*]", "Desstie [*10%*]"];
+const answers = ["99%", "Maybe.", "Certainly not.", "I hope so.", "Not in your wildest dreams.", "There is a good chance.", "Quite likely.", "I think so.", "I hope not.", "I hope so.", "Never!", "Fuhgeddaboudit.", "Ahaha! Really?!?", "Pfft.", "Sorry, bucko.", "Hell, yes.", "Hell, to the na na.", "The future is bleak.", "The future is uncertain.", "I would rather not say.", "Who cares?", "Possibly.", "Never, ever, ever.", "There is a small chance.", "Yes!", "99%", "I Would bet on it."];
 
-//Logging into the bot
+//Outputting to the console.
 
 client.on('ready', () => {
   console.log("Logged in as ${client.user.username}!");
 });
 
 //Logged in, creating a simple test message and we go against trump.
-
-if (msg.content.includes("trump")) {
-    msg.react('👎');
-    msg.reply('Please dont discuss that horrible man. Ever. Like Ever. At all. Thx ;)')
-}
-  
+client.on('message', msg => {
+  if (msg.content.includes("trump")) {
+      msg.react('👎');
+      msg.reply('Please dont discuss that horrible man. Ever. Like Ever. At all. Thx ;)')
+  }
+});  
 //Created an IP command. When you do $IP it shows you the IP of the server.
 
 client.on('message', msg => {
